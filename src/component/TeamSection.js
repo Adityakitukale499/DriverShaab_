3c42f887e383e4b8970793e4c5491b85eb5b8b62
Aditya Kitukale <adityakitukale4599@gmail.com>
2024-03-19 16:47:37 +0530

    update meet our experts

--- src/component/TeamSection.js
+++ src/component/TeamSection.js
@@ -11,103 +11,103 @@ import { useMediaQuery } from '@mui/material';
 const TeamSection = () => {
   const isdescktop = useMediaQuery('1000px')
   return (
-      <div style={{position:'relative'}}>
-         
-      
-    <div style={{width:"100vw", display:"flex",alignItems:"center", justifyContent:"center", position:'relative'}}>
-    <div className="aboutus_text_slash" style={{marginRight:'70px',marginLeft:'-200px', display:isdescktop?'block':'none'}}>
-            <div className="aboutus_blur_heading">Teams</div>
-            <div className="line_img">
-              <img src={line} alt="lineimg" style={{ height: "52px" }} />
-            </div>
+    <div style={{ position: 'relative' }}>
+
+
+      <div style={{ width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", position: 'relative' }}>
+        <div className="aboutus_text_slash" style={{ marginRight: '70px', marginLeft: '-200px', display: isdescktop ? 'block' : 'none' }}>
+          <div className="aboutus_blur_heading">Teams</div>
+          <div className="line_img">
+            <img src={line} alt="lineimg" style={{ height: "52px" }} />
           </div>
-      <BubbleLayout/>
-    <section className="our-team-section style-two" max-width="1200px">
-      <div className="auto-container">
-        <div className="sec-title text-center">
-        {/* <div className="intro-paragraph" style={{height:"fitContent", width: 'auto', margin: '20px auto', fontFamily: 'Inter', fontSize: '20px', fontWeight: 400, lineHeight: '30px', letterSpacing: '0em', textAlign: 'center' }}>
+        </div>
+        <BubbleLayout />
+        <section className="our-team-section style-two" max-width="1200px">
+          <div className="auto-container">
+            <div className="sec-title text-center">
+              {/* <div className="intro-paragraph" style={{height:"fitContent", width: 'auto', margin: '20px auto', fontFamily: 'Inter', fontSize: '20px', fontWeight: 400, lineHeight: '30px', letterSpacing: '0em', textAlign: 'center' }}>
   <h1>Elevating Journeys for 5 Years</h1>
   <p>Drivershaab has been dedicated to providing unparalleled driver services to individuals and businesses across 25 cities, covering an impressive distance of over 5000 km. Our story is one of commitment, reliability, and the unwavering pursuit of excellence.</p>
 </div>
 <img src="img.png" alt="img.png" style={{ maxWidth: '100%', borderRadius: '9px', margin: '20px auto' }} /> */}
 
 
-          <h2>Meet Our Experts</h2>
-          <div className="image-container text-center">
-          
-        </div>
-        </div>
-        <div className="row">
-          {teamMembers.map((member, index) => (
-            <div key={index} className="col-lg-3 col-md-6 col-xs-6 team-block-one">
-              <div className="frame">
-                <div className="inner-box">
-                  <div className="image img_hover_3">
-                    <img src={member.imageSrc} alt={member.name} />
-                  </div>
-                  <div className="lower-content">
-                    <div className="name">{member.name}</div>
-                    <div className="designation">{member.designation}</div>
-                    <ul className="social-links">
-                      {member.socialLinks.map((link, i) => (
-                        <li key={i}>
-                          <a href={link.url}>
-                            <i className={`fab ${link.icon}`}></i>
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-               
+              <h2>Meet Our Experts</h2>
+              <div className="image-container text-center">
+
               </div>
             </div>
-          ))}
-        </div>
+            <div className="row">
+              {teamMembers.map((member, index) => (
+                <div key={index} className="col-lg-3 col-md-6 col-xs-6 team-block-one">
+                  <div className="frame">
+                    <div className="inner-box">
+                      <div className="image img_hover_3">
+                      <img style={{height:'170px'}} src={member.imageSrc} alt={member.name} />
+                      </div>
+                      <div className="lower-content" style={{width:'230px'}}>
+                        <div className="name">{member.name}</div>
+                        <div className="designation">{member.designation}</div>
+                        <ul className="social-links">
+                          {member.socialLinks.map((link, i) => (
+                            <li key={i}>
+                              <a href={link.url} target="_blank">
+                                <i className={`fab ${link.icon}`}></i>
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
-    </div>
     </div>
   );
 };
 
 const teamMembers = [
   {
-    name: 'Sarah Albert',
-    designation: 'Instructor',
-    imageSrc:'image-24.jpg',
+    name: 'Neha Shaikh',
+    designation: 'Head of Procurement',
+    imageSrc: 'neha.jpg',
     socialLinks: [
-      { url: '#', icon: 'fa-twitter' },
+      { url: 'https://www.linkedin.com/in/daisy-saikia-16072211a', icon: 'fa-linkedin' },
       { url: '#', icon: 'fa-facebook-f' },
       { url: '#', icon: 'fa-instagram' },
     ],
   },
   {
-    name: 'David James',
-    designation: 'Instructor',
-    imageSrc:'image-25.jpg',
+    name: 'Arpit Sinha',
+    designation: 'Head of Supply',
+    imageSrc: 'arpit.jpg',
     socialLinks: [
-      { url: '#', icon: 'fa-twitter' },
+      { url: 'https://www.linkedin.com/in/arpit-sinha-60593834', icon: 'fa-linkedin' },
       { url: '#', icon: 'fa-facebook-f' },
       { url: '#', icon: 'fa-instagram' },
     ],
   },
   {
-    name: 'Jessica Brown',
-    designation: 'Instructor',
-    imageSrc:'image-26.jpg',
+    name: 'James Joy',
+    designation: 'Head of Operations',
+    imageSrc: 'jamesh.jpg',
     socialLinks: [
-      { url: '#', icon: 'fa-twitter' },
+      { url: 'https://www.linkedin.com/in/james-joy-a77358241', icon: 'fa-linkedin' },
       { url: '#', icon: 'fa-facebook-f' },
       { url: '#', icon: 'fa-instagram' },
     ],
   },
   {
-    name: 'Kevin Martin',
-    designation: 'Instructor',
-    imageSrc:'image-27.jpg',
+    name: 'Poulami Chaterjee',
+    designation: 'Head of Marketing',
+    imageSrc: 'poulami.jpg',
     socialLinks: [
-      { url: '#', icon: 'fa-twitter' },
+      { url: 'https://www.linkedin.com/in/vama-sethia-16116816b', icon: 'fa-linkedin' },
       { url: '#', icon: 'fa-facebook-f' },
       { url: '#', icon: 'fa-instagram' },
     ],
