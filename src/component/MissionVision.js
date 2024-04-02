2f5574d5f3e39368b2f0196de97b2a737fd5355a
Aditya Kitukale <adityakitukale4599@gmail.com>
2024-04-02 12:14:39 +0530

    add short story

--- src/component/MissionVision.js
+++ src/component/MissionVision.js
@@ -14,23 +14,47 @@ const MissionVision = () => {
             p: 4,
         }}>
             <Box sx={{ maxWidth: '1300px', textAlign: 'center', px: 2 }}>
-                
-            <Typography sx={{ fontFamily: 'Work Sans', fontWeight: 700, fontSize: '36px', lineHeight: '42px', color: '#fff', textAlign: 'center' }}>Welcome to DriverShaab </Typography>
-                <Typography sx={{ fontFamily: 'Work Sans', fontWeight: 400, fontSize: '16px', lineHeight: '26px', color: '#fff', mt: 2, mb:3, fontStyle: 'italic' }}>
+
+                <Typography sx={{
+                    fontFamily: 'Work Sans', fontWeight: 700, fontSize: '36px', lineHeight: '42px', color: '#fff', textAlign: 'center', WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundImage: '-webkit-linear-gradient(#eee, #888)',
+                }}>Welcome to DriverShaab </Typography>
+                <Typography sx={{
+                    fontFamily: 'Work Sans', fontWeight: 400, fontSize: '16px', lineHeight: '26px', color: '#fff', mt: 2, mb: 3, fontStyle: 'italic', WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundImage: '-webkit-linear-gradient(#eee, #fff)',
+                }}>
                     " where convenience meets reliability. " <br />
                     " Our mission is to connect drivers with passengers seamlessly.
                     With a focus on passenger's safety and driver livelihoods ,we empower drivers to maximize their earnings while providing passengers with a reliable and affordable transportation option." <br />
                     " Join us in reshaping urban mobility—one ride at a time." <br />
                     " DriverShaab: Your car, Your Driver. "</Typography>
-                <Typography sx={{ fontFamily: 'Work Sans', fontWeight: 700, fontSize: '36px', lineHeight: '42px', color: '#fff', textAlign: 'center' }}>Our Mission </Typography>
-                <Typography sx={{ fontFamily: 'Work Sans', fontWeight: 400, fontSize: '16px', lineHeight: '26px', color: '#fff', mt: 2, mb:3, fontStyle: 'italic' }}>
+                <Typography sx={{
+                    fontFamily: 'Work Sans', fontWeight: 700, fontSize: '36px', lineHeight: '42px', color: '#fff', textAlign: 'center', WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundImage: '-webkit-linear-gradient(#eee, #888)'
+                }}>Our Mission </Typography>
+                <Typography sx={{
+                    fontFamily: 'Work Sans', fontWeight: 400, fontSize: '16px', lineHeight: '26px', color: '#fff', mt: 2, mb: 3, fontStyle: 'italic', WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundImage: '-webkit-linear-gradient(#eee, #fff)',
+                }}>
                     " India’s Largest Technology enabled Driver Partner Market place "</Typography>
 
 
 
 
-                <Typography sx={{ fontFamily: 'Work Sans', fontWeight: 700, fontSize: '36px', lineHeight: '42px', color: '#fff', textAlign: 'center' }}>Our Vision </Typography>
-                <Typography sx={{ fontFamily: 'Work Sans', fontWeight: 400, fontSize: '16px', lineHeight: '26px', color: '#fff', mt: 2, mb:3, fontStyle: 'italic' }}>
+                <Typography sx={{
+                    fontFamily: 'Work Sans', fontWeight: 700, fontSize: '36px', lineHeight: '42px', color: '#fff', textAlign: 'center', WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundImage: '-webkit-linear-gradient(#eee, #888)'
+                }}>Our Vision </Typography>
+                <Typography sx={{
+                    fontFamily: 'Work Sans', fontWeight: 400, fontSize: '16px', lineHeight: '26px', color: '#fff', mt: 2, mb: 3, fontStyle: 'italic', WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    backgroundImage: '-webkit-linear-gradient(#eee, #fff)',
+                }}>
                     " To continuously improve, innovate and revolutionize the Driver partner ecosystem "</Typography>
             </Box>
         </Box>
